32e70cbf498eb00fe018bbd4f68cac3d43481ded
Heitor Ferreira <heitorfr@gmail.com>
2014-01-25 14:05:56 +0000

    Initial Commit

---
+++ fract/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  fract
+//
+//  Created by Heitor Ferreira on 25/01/14.
+//  Copyright (c) 2014 Heitor Ferreira. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
